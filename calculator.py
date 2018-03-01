d8afbe184a43bf5809bf24cc9f7db722506b0487
gautam <gautimeena456@gmail.com>
2018-03-01 23:32:55 +0530

    test purpose

---
+++ calculator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+x=input("Enter a no:")
+y=input("Enter another no:")
+
+print(x+y)
+i=1;
+i = range(0,10)
+print(i)
+
+i.append(10)
+i.insert(2,"kucch bhi")
+
+print(i)
+"""print("\n",i)"""
+
+
+def add():
+    print(2+3)
+
